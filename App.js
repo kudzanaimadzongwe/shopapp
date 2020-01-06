7d9faf996c24c92be5defa55f826e75ed3d8506b
kudzanaimadzongwe <kudziemadzongwe6@gmail.com>
2020-01-06 16:44:55 +0200

    commit

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import { AppLoading } from "expo";
 import * as Font from "expo-font";
 
 import productReducer from "./store/reducers/products";
+import cartReducer from "./store/reducers/cart";
 import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
-  products: productReducer
+  products: productReducer,
+  cart: cartReducer
 });
 
 const store = createStore(rootReducer);
