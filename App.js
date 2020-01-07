3f41a7c433d5682420e3da736bf72bad34724c22
kudzanaimadzongwe <kudziemadzongwe6@gmail.com>
2020-01-07 15:02:37 +0200

    commit

--- App.js
+++ App.js
@@ -6,11 +6,13 @@ import * as Font from "expo-font";
 
 import productReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
+import ordersReducer from "./store/reducers/orders";
 import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
   products: productReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  orders: ordersReducer
 });
 
 const store = createStore(rootReducer);
