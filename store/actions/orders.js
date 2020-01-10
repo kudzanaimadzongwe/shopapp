898250ba80ae54a1044468ffe043d047758be342
kudzanaimadzongwe <kudziemadzongwe6@gmail.com>
2020-01-10 13:51:27 +0200

    commit

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -36,10 +36,11 @@ export const fetchOrders = () => {
 };
 
 export const addOrder = (cartItems, totalAmount) => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const date = new Date();
     const response = await fetch(
-      "https://rn-shop-app-57101.firebaseio.com/orders/u1.json",
+      `https://rn-shop-app-57101.firebaseio.com/orders/u1.json?auth=${token}`,
       {
         method: "POST",
         headers: {
